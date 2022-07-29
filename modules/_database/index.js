9ef330e7676a998307cba81893c1a0982efbd9d1
silvestre149-bit <74218849+silvestre149-bit@users.noreply.github.com>
2022-07-29 12:18:09 -0300

    Update index.js

--- modules/_database/index.js
+++ modules/_database/index.js
@@ -18,8 +18,7 @@ switch (process.env.NODE_ENV) {
 }
 
 
-const URL = 'mongodb+srv://admin:admin@cluster0.cccmi.mongodb.net/?retryWrites=true&w=majority' + DB_NAME
-
+const URL = 'mongodb+srv://admin:admin@cluster0.cccmi.mongodb.net/?retryWrites=true&w=majority' 
 mongoose.connect(URL)
     .catch(err => {
         throw 'Banco não conseguiu se conectar'
